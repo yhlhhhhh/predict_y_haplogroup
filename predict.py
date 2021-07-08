d1ed3cdab572ab1cbc10a76fedb4e8aa7d71dc8f
yhlhhhhh <67506574+yhlhhhhh@users.noreply.github.com>
2021-07-08 16:53:50 +0800

    Update predict.py

--- predict.py
+++ predict.py
@@ -4,8 +4,9 @@ import pandas as pd
 
 
 file_name = input('Please enter your Y-STR file name:')
+model = input('Please enter a model name:')
 df = pd.read_csv(f'{file_name}.csv', index_col = 0, header = 0)
 feature_data = df.columns.tolist()
 x = df[feature_data].values
-rfc = joblib.load('')
+rfc = joblib.load('f{os.getcwd()}predict_y_haplogroup/model/{model}')
 print(rfc.predict(x))
