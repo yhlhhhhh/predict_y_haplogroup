e10035cf3cb18713279c192fe08d7b8ca24f2436
yhlhhhhh <67506574+yhlhhhhh@users.noreply.github.com>
2021-07-08 16:55:22 +0800

    Update predict.py

--- predict.py
+++ predict.py
@@ -8,5 +8,5 @@ model = input('Please enter a model name:')
 df = pd.read_csv(f'{file_name}.csv', index_col = 0, header = 0)
 feature_data = df.columns.tolist()
 x = df[feature_data].values
-rfc = joblib.load('f{os.getcwd()}predict_y_haplogroup/model/{model}')
+rfc = joblib.load(f'{os.getcwd()}predict_y_haplogroup/model/{model}')
 print(rfc.predict(x))
