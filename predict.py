de8f3c1be73406b52bc5c34a78db1f8cacca5474
yhlhhhhh <67506574+yhlhhhhh@users.noreply.github.com>
2021-07-08 16:50:24 +0800

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,12 +1,11 @@
 import os
 import joblib
 import pandas as pd
-from sklearn.ensemble import RandomForestClassifier
 
 
 file_name = input('Please enter your Y-STR file name:')
 df = pd.read_csv(f'{file_name}.csv', index_col = 0, header = 0)
 feature_data = df.columns.tolist()
 x = df[feature_data].values
-rfc = RandomForestClassifier()
+rfc = joblib.load('')
 print(rfc.predict(x))
